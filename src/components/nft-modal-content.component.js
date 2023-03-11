3213505ba0da5538680b750f25703843a59ec191
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-03-11 10:37:50 +0100

    Done with card rendering issues

--- src/components/nft-modal-content.component.js
+++ src/components/nft-modal-content.component.js
@@ -6,8 +6,8 @@ export const ModalContentComponent = ({nft}) => {
         <div className="nft-modal-info">
             <p><h2>{nft.name}</h2></p>
             <p>{nft.price}</p>
-            <p><a href="#" target="__blank">Owner</a></p>
-            <p><a href="#">Collection</a></p>
+            <p><a href={nft.ownerLink} target="__blank">Owner</a></p>
+            <p><a href={nft.collectionLink} target="__blank">Collection</a></p>
             <p><a href={nft.verifyLink} target="__blank">Verify</a></p>
             <p><button onClick={()=>{ window.open(nft.link, "__blank") }}>Buy Now</button></p>
         </div>
